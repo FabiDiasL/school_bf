bc67ef31bd72507961933c37fa0339813bc0c04c
FabiDiasL <fabi_lima_@hotmail.com>
2023-03-13 13:21:53 -0300

    first commit

---
+++ school_bf/src/main/java/br/com/xavecoding/school_bf/SchoolBfApplication.java
@@ -0,0 +1,13 @@
+package br.com.xavecoding.school_bf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolBfApplication.class, args);
+	}
+
+}
---
+++ school_bf/src/test/java/br/com/xavecoding/school_bf/SchoolBfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xavecoding.school_bf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
