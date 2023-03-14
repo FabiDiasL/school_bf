d061ec754b58f19ac855ca2a48913e6eb4735752
FabiDiasL <fabi_lima_@hotmail.com>
2023-03-14 13:17:21 -0300

    update

--- src/main/java/br/com/xavecoding/school_bf/SchoolBfApplication.java
+++ src/main/java/br/com/xavecoding/school_bf/SchoolBfApplication.java
@@ -9,5 +9,4 @@ public class SchoolBfApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolBfApplication.class, args);
 	}
-
 }
---
+++ src/main/java/br/com/xavecoding/school_bf/orm/Professor.java
@@ -0,0 +1,21 @@
+package br.com.xavecoding.school_bf.orm;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "professores")
+public class Professor {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String nome;
+	@Column(nullable = false, unique = true)
+	private String matricula;
+}
