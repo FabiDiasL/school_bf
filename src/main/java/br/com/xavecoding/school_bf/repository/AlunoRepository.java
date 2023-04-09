b1ea97f7f10a8ca39d05b7f50f8a2c27e6adeba4
FabiDiasL <fabi_lima_@hotmail.com>
2023-04-08 22:20:14 -0300

    update

--- src/main/java/br/com/xavecoding/school_bf/repository/AlunoRepository.java
+++ src/main/java/br/com/xavecoding/school_bf/repository/AlunoRepository.java
@@ -1,8 +1,11 @@
 package br.com.xavecoding.school_bf.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.xavecoding.school_bf.orm.Aluno;
 
 public interface AlunoRepository extends CrudRepository<Aluno, Long> {
+	public abstract List<Aluno> findByName(String nome);
 }
